c3dec1863f69c3dcedbbb38ec7c9e7e3ceebde6d
lingandken <64186033+lingandken@users.noreply.github.com>
2022-04-27 13:35:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt 21.02-SNAPSHOT/Build $(TZ=UTC-8 date "+%Y.%m.%d") By JJ/g' package/default-settings/files/zzz-default-settings
